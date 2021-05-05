b7426620c89d8f22ec9d57feeaf6f258685b8954
Valter Fernandes <valter@digi4ind.com>
2021-05-05 09:00:25 -0300

    Project created

---
+++ backend/src/main/java/com/salesdash/SalesdashApplication.java
@@ -0,0 +1,13 @@
+package com.salesdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesdashApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/salesdash/SalesdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
